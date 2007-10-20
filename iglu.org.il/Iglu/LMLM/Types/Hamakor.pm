12bc780e3346cd53c59f0966380ac4b73310580d
shlomif <shlomif@124812d1-1e0d-29c7-c697-926a49d246de>
2007-10-20 14:02:40 +0000

    Fixed the Hamakor mailing list type.

--- iglu.org.il/Iglu/LMLM/Types/Hamakor.pm
+++ iglu.org.il/Iglu/LMLM/Types/Hamakor.pm
@@ -2,17 +2,24 @@ package Iglu::LMLM::Types::Hamakor;
 
 use strict;
 
-use Iglu::LMLM::Types::Iglu;
+use Mail::LMLM::Types::Mailman;
 
 use vars qw(@ISA);
 
-@ISA=qw(Iglu::LMLM::Types::Iglu);
+@ISA=qw(Mail::LMLM::Types::Mailman);
 
 sub get_default_hostname
 {
     my $self = shift;
 
     return "hamakor.org.il";
+}
+
+sub get_maintenance_url
+{
+    my $self = shift;
+
+    return "http://hamakor.org.il/cgi-bin/mailman/listinfo/" . $self->get_group_base();
 }
 
 1;
