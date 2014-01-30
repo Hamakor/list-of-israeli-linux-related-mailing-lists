cd90f371281c2852ce2dec16c7ef93b3c89175ff
Shlomi Fish <shlomif@shlomifish.org>
2014-01-30 12:14:58 +0200

    Add another upload link.

--- iglu.org.il/upload.sh
+++ iglu.org.il/upload.sh
@@ -2,4 +2,5 @@
 RSYNC="rsync --rsh=ssh -a -v --progress --delete-after"
 # $RSYNC mailing-lists shlomif@iglu.org.il:/iglu/html/
 $RSYNC mailing-lists "${HOMEPAGE_SSH_PATH}"/Iglu/
+$RSYNC mailing-lists "${__HOMEPAGE_REMOTE_PATH}"/Iglu/
 $RSYNC mailing-lists gnu.hamakor.org.il:/home/www/iglu.org.il/
